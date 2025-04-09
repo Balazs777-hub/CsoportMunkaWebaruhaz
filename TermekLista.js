ae5c9b72c5870a8b46112b47d7f70bb0d704d916
Bori888 <ilyes.bori8@gmail.com>
2025-04-09 13:42:30 +0200

    Kosar kezdet

--- TermekLista.js
+++ TermekLista.js
@@ -1,36 +1,39 @@
-import { termekLista } from "./TermekAdatok.js";
+import { Kosar } from './Kosar.js'; 
 
-export class TermekLista{
-    #carCardsContainer;
-    #termekLista= []
-    constructor(){
-        this.#termekLista = termekLista
-        this.#carCardsContainer = document.getElementById('carCards');
-        this.megjelenit();
-    }
+export class TermekLista {
+  #carCardsContainer;
+  kosar; 
 
-    megjelenit(){
-    // Iteráljuk a termékeket és létrehozzuk a kártyákat
-    this.#termekLista.forEach((termek) => {
-    const card = document.createElement('div');
-    card.classList.add('col-md-4', 'mb-4');
-  
-    card.innerHTML = `
+  constructor() {
+    this.#carCardsContainer = document.getElementById('carCards');
+    this.kosar = new Kosar(); 
+    this.megjelenit();
+  }
+
+  megjelenit() {
+    termekLista.forEach((termek) => {
+      const card = document.createElement('div');
+      card.classList.add('col-md-4', 'mb-4');
+
+      card.innerHTML = `
         <div class="card">
-            <img src="${termek.kep}" class="card-img-top" alt="${termek.nev}">
-            <div class="card-body">
+          <img src="${termek.kep}" class="card-img-top" alt="${termek.nev}">
+          <div class="card-body">
             <h5 class="card-title">${termek.nev}</h5>
             <p class="card-text">${termek.leiras}</p>
             <p class="card-text"><strong>Ár: ${termek.Ar} Ft</strong></p>
-            <button class="btn btn-primary">Kosárba rakom</button>
-            </div>
+            <button class="btn btn-primary kosarBtn">Kosárba rakom</button>
+          </div>
         </div>
-        `;
-    
-        this.#carCardsContainer.appendChild(card);
+      `;
+
+      
+      this.kartyaHozzaAd(card, termek);
+
+      this.#carCardsContainer.appendChild(card);
     });
-    }
+  }
 
+  
+  
 }
-
-
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { Kosar } from "./Kosar.js";
 import { TermekLista } from "./TermekLista.js";
 
-const egyTermekLista = new TermekLista(); 
+
+const egyTermekLista = new TermekLista();
+const egyKosar =new Kosar();
