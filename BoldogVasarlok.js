30a2918cdf8f9f230aa08daded387334c7eb3a1f
Balazs777-hub <gbalazs22@gmail.com>
2025-04-09 12:37:36 +0200

    Wow upgrade

---
+++ BoldogVasarlok.js
@@ -0,0 +1,39 @@
+export default class Vasarlok {
+
+    constructor() {
+        this.megjelenit();
+    }
+
+    megjelenit(){
+
+        const card = document.querySelector(".boldogV")
+
+        card.innerHTML =`
+        <div id="carouselExampleIndicators" class="carousel slide">
+            <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="Kepek/costumer4.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="Kepek/costumer2.webp" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="Kepek/costumer3.webp" class="d-block w-100" alt="...">
+            </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"></span>
+            </button>
+        </div>`
+    }
+}
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import { TermekLista } from "./TermekLista.js";
+import Vasarlok from "./BoldogVasarlok.js";
 
 const egyTermekLista = new TermekLista(); 
+const egyVasarlok = new Vasarlok();
