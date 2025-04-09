039542449678caa66b8c4a1f660703d2010628dc
Balazs777-hub <gbalazs22@gmail.com>
2025-04-09 10:58:15 +0200

    asd

--- TermekekLista.js
+++ TermekekLista.js
@@ -1,33 +1,64 @@
 const termekLista = [
     {
-      kep: "https://example.com/car1.jpg",
+      kep: "Kepek/tesla.png",
       nev: "Tesla Model S",
       leiras: "A teljesen elektromos luxusautó, amely forradalmasította a jövőt.",
       Ar: "120,000 USD"
     },
     {
-      kep: "https://example.com/car2.jpg",
+      kep: "Kepek/m3.webp",
       nev: "BMW 3 Series",
       leiras: "A prémium kategóriás középkategóriás sedan, sportos vezetési élménnyel.",
       Ar: "45,000 USD"
     },
     {
-      kep: "https://example.com/car3.jpg",
-      nev: "Audi A4",
+      kep: "Kepek/audi.png",
+      nev: "Audi A3",
       leiras: "Elegáns és technológiai szempontból fejlett autó, amely a luxust és kényelmet ötvözi.",
-      Ar: "48,000 USD"
+      Ar: "38,000 USD"
     },
     {
-      kep: "https://example.com/car4.jpg",
+      kep: "Kepek/merci.jpg",
       nev: "Mercedes-Benz C-Class",
       leiras: "Kiváló minőségű, kényelmes és biztonságos autó a prémium kategóriában.",
       Ar: "55,000 USD"
     },
     {
-      kep: "https://example.com/car5.jpg",
-      nev: "Toyota Camry",
-      leiras: "Megbízható és kényelmes sedan, amely hosszú távon is gazdaságos választás.",
-      Ar: "30,000 USD"
+      kep: "Kepek/chevrolet.jpg",
+      nev: "Chevrolet Corvete",
+      leiras: "Elegáns még is robosztusos megjelenésü izom autó, melyben a vezetés egy külön élmény.",
+      Ar: "130,000 USD"
+    },
+    {
+      kep: "Kepek/cadillac
+      .jpg",
+      nev: "Cadillac XT4",
+      leiras: "Amerikai izomautó, amely kiemelkedő teljesítményt és stílust kínál.",
+      Ar: "50,000 USD"
+    },
+    {
+      kep: "Kepek/bmw x3.png",
+      nev: "BMW X3m50i",
+      leiras: "Teljesen elektromos autó, amely környezetbarát és gazdaságos.",
+      Ar: "35,000 USD"
+    },
+    {
+      kep: "https://example.com/car8.jpg",
+      nev: "Honda Accord",
+      leiras: "Stílusos és megbízható középkategóriás autó, amely kényelmet és gazdaságosságot kínál.",
+      Ar: "28,000 USD"
+    },
+    {
+      kep: "https://example.com/car9.jpg",
+      nev: "Porsche 911",
+      leiras: "A sportautók királya, amely tökéletes vezetési élményt biztosít.",
+      Ar: "100,000 USD"
+    },
+    {
+      kep: "https://example.com/car10.jpg",
+      nev: "Nissan Leaf",
+      leiras: "Egy kompakt elektromos autó, amely környezetbarát és megfizethető.",
+      Ar: "32,000 USD"
     },
     {
       kep: "https://example.com/car6.jpg",
