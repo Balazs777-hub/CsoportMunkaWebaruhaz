35995c4cef0848262a3eaaa8fc5a413663daefef
Balazs777-hub <gbalazs22@gmail.com>
2025-04-09 11:17:19 +0200

    ListaKesz

--- TermekekLista.js
+++ TermekekLista.js
@@ -3,67 +3,67 @@ const termekLista = [
       kep: "Kepek/tesla.png",
       nev: "Tesla Model S",
       leiras: "A teljesen elektromos luxusautó, amely forradalmasította a jövőt.",
-      Ar: "120,000 USD"
+      Ar: 25000000,
     },
     {
       kep: "Kepek/m3.webp",
       nev: "BMW 3 Series",
       leiras: "A prémium kategóriás középkategóriás sedan, sportos vezetési élménnyel.",
-      Ar: "45,000 USD"
+      Ar: 17000000,
     },
     {
       kep: "Kepek/audi.png",
       nev: "Audi A3",
       leiras: "Elegáns és technológiai szempontból fejlett autó, amely a luxust és kényelmet ötvözi.",
-      Ar: "38,000 USD"
+      Ar: 10000000,
     },
     {
       kep: "Kepek/merci.jfiff",
       nev: "Mercedes-Benz C-Class",
       leiras: "Kiváló minőségű, kényelmes és biztonságos autó a prémium kategóriában.",
-      Ar: "55,000 USD"
+      Ar: 50000000,
     },
     {
       kep: "Kepek/chevrolet.jpg",
       nev: "Chevrolet Corvete",
       leiras: "Elegáns még is robosztusos megjelenésü izom autó, melyben a vezetés egy külön élmény.",
-      Ar: "130,000 USD"
+      Ar: 70000000,
     },
     {
       kep: "Kepek/cadillac.png",
       nev: "Cadillac XT4",
       leiras: "Amerikai izomautó, amely kiemelkedő teljesítményt és stílust kínál.",
-      Ar: "50,000 USD"
+      Ar: 35000000,
     },
     {
       kep: "Kepek/bmw x3.png",
       nev: "BMW X3m50i",
       leiras: "Teljesen elektromos autó, amely környezetbarát és gazdaságos.",
-      Ar: "35,000 USD"
+      Ar: 17000000,
     },
     {
       kep: "Kepek/bentley2.png",
       nev: "Bentley Continental",
       leiras: "A teljesen elektromos luxusautó, amely forradalmasította a jövőt.",
-      Ar: "170,000 USD"
+      Ar: 90000000,
     },
     {
       kep: "Kepek/bentley.png",
       nev: "Bentley Bentayga",
       leiras: "A prémium kategóriás SUV, sportos vezetési élménnyel.",
-      Ar: "145,000 USD"
+      Ar: 100000000,
     },
     {
       kep: "Kepek/audi.png",
       nev: "Audi A3",
       leiras: "Elegáns és technológiai szempontból fejlett autó, amely a luxust és kényelmet ötvözi.",
-      Ar: "38,000 USD"
+      Ar: 7000000,
     },
     {
       kep: "Kepek/merci.jpg",
       nev: "Mercedes-Benz C-Class",
       leiras: "Kiváló minőségű, kényelmes és biztonságos autó a prémium kategóriában.",
-      Ar: "55,000 USD"
+      Ar: 33000000,
     },
   ];
   
